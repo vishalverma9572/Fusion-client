7545b9f9bf3e9e278e590511cc385f3631b34134
jainvedant392 <jainvedant392@gmail.com>
2024-10-03 01:30:00 +0530

    Created Login Page and Forgot Password Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-import { Layout } from './components/layout.jsx';
-import Dashboard from './Modules/Dashboard/dashboardContent.jsx';
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components/layout.jsx";
+import Dashboard from "./Modules/Dashboard/dashboardContent.jsx";
+import LoginPage from "./pages/login.jsx";
+import ForgotPassword from "./pages/forgotPassword.jsx";
 
 export default function App() {
-  return <MantineProvider>
-    <Layout>
-      <Dashboard />
-    </Layout>
-  </MantineProvider>;
+  return (
+    <MantineProvider>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Dashboard />
+            </Layout>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/reset-password" element={<ForgotPassword />} />
+      </Routes>
+    </MantineProvider>
+  );
 }
---
+++ src/pages/forgotPassword.jsx
@@ -0,0 +1,51 @@
+import {
+  Button,
+  Center,
+  Container,
+  Divider,
+  Paper,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
+
+const ForgotPassword = () => {
+  return (
+    <Center w="100%">
+      <Container w={420} my={100}>
+        <Title ta="center">Reset Password</Title>
+
+        <Paper
+          withBorder
+          shadow="lg"
+          p={30}
+          mt={40}
+          radius="md"
+          style={{ border: "2px solid #15ABFF" }}
+        >
+          <Text size="sm">
+            Forgotten your password? Enter your e-mail address below, and we
+            will send you an e-mail allowing you to reset it.
+          </Text>
+          <Divider my="md" />
+          <TextInput
+            label="Username/Email"
+            type="email"
+            placeholder="username or email"
+            required
+          />
+          <Button fullWidth mt="xl" bg="#15ABFF">
+            Reset Password
+          </Button>
+          <Divider my="md" />
+          <Text size="sm">
+            Please contact CC admin if you have any trouble resetting your
+            password.
+          </Text>
+        </Paper>
+      </Container>
+    </Center>
+  );
+};
+
+export default ForgotPassword;
