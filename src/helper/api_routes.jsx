7fb36a17b61bed3491c972bc840425af6618c6b3
uphadeharsh45 <128834201+uphadeharsh45@users.noreply.github.com>
2024-10-03 15:15:48 +0530

    fetched user roles, notifications and announcements

--- src/helper/api_routes.jsx
+++ src/helper/api_routes.jsx
@@ -3,3 +3,4 @@ export const host = "http://127.0.0.1:8000";
 // Rest-API Endpoints
 export const loginRoute = `${host}/api/auth/login/`;
 export const logoutRoute = `${host}/api/auth/logout/`;
+export const dashboardRoute = `${host}/api/dashboard/`;
