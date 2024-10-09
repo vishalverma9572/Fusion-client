e6692d2246b5f0bb21fb91161975c99b59cf935e
Vishal Kumar <vishalverma9572@gmail.com>
2024-10-10 03:32:47 +0530

    HR module initial commit as all the initial setup is done

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import LoginPage from "./pages/login";
 import ForgotPassword from "./pages/forgotPassword";
 import AcademicPage from "./Modules/Academic/index";
 import ValidateAuth from "./helper/validateauth";
+import HR from "./Modules/HR/index";
 
 export default function App() {
   const location = useLocation();
@@ -45,6 +46,14 @@ export default function App() {
           element={
             <Layout>
               <Profile />
+            </Layout>
+          }
+        />
+        <Route
+          path="/hr"
+          element={
+            <Layout>
+              <HR />
             </Layout>
           }
         />
---
+++ src/Modules/HR/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HR() {
+  return (
+    <>
+      <h1>HR MODULE</h1>
+      <p>HR Module is under construction</p>
+    </>
+  );
+}
