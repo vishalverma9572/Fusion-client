7784681e32c99d7dcf3caf0066dc60787f264843
Joyee2004 <joyee6biswas@gmail.com>
2024-10-11 00:36:32 +0530

    modified index.jsx for leaveForm display

--- src/Modules/HR/index.jsx
+++ src/Modules/HR/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-
+import LeaveForm from "./pages/leaveForm";
+import { MantineProvider } from '@mantine/core';
 export default function HR() {
   return (
-    <>
-      <h1>HR MODULE</h1>
-      <p>HR Module is under construction</p>
-    </>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <div>
+        <h1>HR Module</h1>
+        <LeaveForm />
+      </div>
+    </MantineProvider>
   );
 }
