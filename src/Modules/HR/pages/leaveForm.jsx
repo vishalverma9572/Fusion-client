11f789219aa274194f495471918cc84f3be70a2e
Geetika09 <geetikakh2003@gmail.com>
2024-10-15 12:12:27 +0530

    CpdaForm Updated with the current UI

--- src/Modules/HR/pages/leaveForm.jsx
+++ src/Modules/HR/pages/leaveForm.jsx
@@ -1,17 +1,31 @@
 import React from "react";
-import { Button } from "@mantine/core";
-import { PaperPlaneRight, CheckCircle, User, Tag, IdentificationCard, Building, Calendar, ClipboardText } from "@phosphor-icons/react";
+import { Button, Select } from "@mantine/core";
+import {
+  PaperPlaneRight,
+  CheckCircle,
+  User,
+  Tag,
+  IdentificationCard,
+  Building,
+  Calendar,
+  ClipboardText,
+  FloppyDisk,
+} from "@phosphor-icons/react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateForm, resetForm } from "../../../redux/formSlice";
-import "../styles/leaveForm.css";
+import "./leaveForm.css";
 
 const LeaveForm = () => {
   const formData = useSelector((state) => state.form);
   const dispatch = useDispatch();
 
   const handleChange = (event) => {
-    const { name, value } = event.currentTarget;
+    const { name, value } = event.target;
     dispatch(updateForm({ name, value }));
+  };
+
+  const handleSelectChange = (value) => {
+    dispatch(updateForm({ name: "natureOfLeave", value }));
   };
 
   const handleSubmit = (event) => {
@@ -21,11 +35,11 @@ const LeaveForm = () => {
   };
 
   return (
-    <div className="container">
+    <div className="Leave_container">
       <form onSubmit={handleSubmit}>
-        {/* Row 1: Name and Designation */}
+        {/* Section 1: Name, Designation (Left), Application Date (Right) */}
         <div className="grid-row">
-          <div className="grid-col">
+          <div className="grid-col left-side">
             <label className="input-label" htmlFor="name">
               Name
             </label>
@@ -42,8 +56,6 @@ const LeaveForm = () => {
                 required
               />
             </div>
-          </div>
-          <div className="grid-col">
             <label className="input-label" htmlFor="designation">
               Designation
             </label>
@@ -61,28 +73,28 @@ const LeaveForm = () => {
               />
             </div>
           </div>
-        </div>
 
-        {/* Row 2: PF Number and Department */}
-        <div className="grid-row">
-          <div className="grid-col">
-            <label className="input-label" htmlFor="pfNumber">
-              PF Number
+          <div className="grid-col right-side">
+            <label className="input-label" htmlFor="applicationDate">
+              Application Date
             </label>
-            <div className="input-wrapper">
-              <IdentificationCard size={20} />
+            <div className="input-wrapper center" style={{ width: "300px" }}>
+              <Calendar size={20} />
               <input
-                type="text"
-                id="pfNumber"
-                name="pfNumber"
-                placeholder="XXXXXXXXXXXX"
-                value={formData.pfNumber}
+                type="date"
+                id="applicationDate"
+                name="applicationDate"
+                value={formData.applicationDate}
                 onChange={handleChange}
-                className="input"
+                className="input center"
                 required
               />
             </div>
           </div>
+        </div>
+
+        {/* Section 2: Discipline/Department (Left), PF Number (Right) */}
+        <div className="grid-row">
           <div className="grid-col">
             <label className="input-label" htmlFor="department">
               Department
@@ -101,10 +113,79 @@ const LeaveForm = () => {
               />
             </div>
           </div>
+
+          <div className="grid-col">
+            <label className="input-label" htmlFor="pfNumber">
+              PF Number
+            </label>
+            <div className="input-wrapper">
+              <IdentificationCard size={20} />
+              <input
+                type="number"
+                id="pfNumber"
+                name="pfNumber"
+                placeholder="XXXXXXXXXXXX"
+                value={formData.pfNumber}
+                onChange={handleChange}
+                className="input"
+                required
+              />
+            </div>
+          </div>
         </div>
 
-        {/* Row 3: Leave Start Date and End Date */}
-        <div className="grid-row">
+        {/* Section 3: Nature of Leave, Leave Start Date, Leave End Date */}
+        <div className="grid-row three-columns">
+          <div className="grid-col">
+            <label className="input-label" htmlFor="natureOfLeave">
+              Nature of Leave
+            </label>
+            <div className="input-wrapper">
+              <Select
+                className="select"
+                placeholder="Select a leave type"
+                data={[
+                  { value: "Casual", label: "Casual" },
+
+                  { value: "Vacation", label: "Vacation" },
+                  { value: "Earned", label: "Earned" },
+                  { value: "Commuted Leave", label: "Commuted Leave" },
+                  {
+                    value: "Special Casual Leave",
+                    label: "Special Casual Leave",
+                  },
+                  { value: "Restricted Holiday", label: "Restricted Holiday" },
+                  { value: "Station Leave", label: "Station Leave" },
+                ]}
+                value={formData.natureOfLeave}
+                onChange={handleSelectChange}
+                required
+                styles={{
+                  input: {
+                    border: "none",
+                    backgroundColor: "transparent",
+                    color: "#000",
+                    fontSize: "14px",
+                    margin: "-8px 0px 0px -40px",
+                    fontFamily: "Roboto, sans-serif",
+                  },
+                  dropdown: {
+                    backgroundColor: "#fff",
+                    border: "1px solid #ccc",
+                    borderRadius: "4px",
+                    boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
+                  },
+                  item: {
+                    padding: "10px",
+                    fontSize: "14px",
+                    color: "#2d3b45",
+                    ":hover": { backgroundColor: "#e2e8f0", color: "#1a2a33" },
+                  },
+                }}
+              />
+            </div>
+          </div>
+
           <div className="grid-col">
             <label className="input-label" htmlFor="startDate">
               Leave Start Date
@@ -122,6 +203,7 @@ const LeaveForm = () => {
               />
             </div>
           </div>
+
           <div className="grid-col">
             <label className="input-label" htmlFor="endDate">
               Leave End Date
@@ -141,9 +223,9 @@ const LeaveForm = () => {
           </div>
         </div>
 
-        {/* Row 4: Purpose and Nature of Leave */}
+        {/* Section 4: Purpose of Leave */}
         <div className="grid-row">
-          <div className="grid-col">
+          <div className="grid-col full-width">
             <label className="input-label" htmlFor="purpose">
               Purpose
             </label>
@@ -157,30 +239,14 @@ const LeaveForm = () => {
                 value={formData.purpose}
                 onChange={handleChange}
                 className="input"
+                aria-rowcount={2}
                 required
               />
             </div>
           </div>
-          <div className="grid-col">
-            <label className="input-label" htmlFor="natureOfLeave">
-              Nature of Leave
-            </label>
-            <select
-              id="natureOfLeave"
-              name="natureOfLeave"
-              value={formData.natureOfLeave}
-              onChange={handleChange}
-              className="select"
-              required
-            >
-              <option value="Casual">Casual</option>
-              <option value="Sick">Sick</option>
-              <option value="Earned">Earned</option>
-            </select>
-          </div>
         </div>
 
-        {/* Row 5: Academic Responsibility and Administrative Responsibility */}
+        {/* Section 5: Academic and Administrative Responsibility */}
         <div className="grid-row">
           <div className="grid-col">
             <label className="input-label" htmlFor="academicResponsibility">
@@ -200,6 +266,7 @@ const LeaveForm = () => {
               />
             </div>
           </div>
+
           <div className="grid-col">
             <label
               className="input-label"
@@ -226,7 +293,7 @@ const LeaveForm = () => {
         {/* Footer */}
         <div className="footer-section">
           <div className="input-wrapper">
-           
+            <User size={20} />
             <input
               type="text"
               name="username"
@@ -238,7 +305,7 @@ const LeaveForm = () => {
             />
           </div>
           <div className="input-wrapper">
-           
+            <Tag size={20} />
             <input
               type="text"
               name="designationFooter"
@@ -249,18 +316,17 @@ const LeaveForm = () => {
               required
             />
           </div>
-          <Button
-            leftIcon={<CheckCircle size={20} />}
-            className="button"
-          >
-            Check
+          <Button leftIcon={<CheckCircle size={20} />} className="button">
+            <CheckCircle size={18} /> &nbsp; Check
           </Button>
           <Button
             type="submit"
             rightIcon={<PaperPlaneRight size={20} />}
+            style={{ marginLeft: "250px", width: "150px" }}
             className="button"
           >
-            Submit
+            <FloppyDisk size={20} />
+            &nbsp; Submit
           </Button>
         </div>
       </form>
