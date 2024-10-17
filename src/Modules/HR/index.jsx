2289f3c4fded3e04e34017d6c2f141d83a97f12f
susim-gautam <susimgautam123@gmail.com>
2024-10-18 02:26:07 +0530

    Reverted changes to index.jsx from commit 0314b3003b2bdea878804c4fcf4e7c781ce01d4d

--- src/Modules/HR/index.jsx
+++ src/Modules/HR/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LeavePage from "./pages/LeavePage"; // Adjust the import path if necessary
-import LeaveFormView from "./pages/LeavePageComp/LeaveFormView"; // Adjust the import path if necessary
 import LeaveArchive from "./pages/LeavePageComp/LeaveArchive";
 import CPDA_ADVANCE from "./pages/CPDA_ADVANCE";
 import LTC from "./pages/LTC";
@@ -17,7 +16,6 @@ export default function HR() {
 
       {/* Render LeavePage at /hr/leave */}
       <Route path="leave" element={<LeavePage />} />
-      <Route path="LeaveFromView" element={<LeaveFormView />} />
       <Route path="cpda_adv" element={<CPDA_ADVANCE />} />
       <Route path="ltc" element={<LTC />} />
       <Route path="appraisal" element={<Appraisal />} />
